a1c2838508be47b7d1b44b80a6d7423db1e9f712
Felipe Gutierrez Romero <felipegutiromero@gmail.com>
2021-02-08 14:43:18 -0500

    Cambio de fondo, seccion de trivia (terminada), seccion de musica (en progreso)

--- js/script.js
+++ js/script.js
@@ -3,11 +3,14 @@ $( document ).ready(function() {
 
     const scenario_titles = 
     [
-    	"Bienvenida"
+    	"Bienvenida",
+    	"¿Lista?",
+    	"Salón de la trivia",
+    	"Trivia 1", "Trivia 2", "Trivia 3", "Trivia 4",
+    	"Salón de la música", "Kpop", "Albúm recuerdo"
     ]
 
     if(scenario_number == 1){
-    	console.log("Hola")
     	$("#scenario_1").css("display", "flex");
     	$("#header_title").html("");
     	$("#header_title").html(scenario_titles[scenario_number - 1]);
@@ -18,5 +21,66 @@ $( document ).ready(function() {
     	scenario_number++;
     	$("#scenario_" + scenario_number).css("display", "flex");
     	$("#scenario_" + (scenario_number - 1)).css("display", "none");
+    	$("#header_title").html("");
+    	$("#header_title").html(scenario_titles[scenario_number - 1]);
+    });
+
+    //Trivia
+    $('.pregunta').click(function(){
+    	if($(this).attr("valor") == "1"){
+    		$('.scenario_popup_container').css("display", "flex");
+    		var image_result = '<img src="./img/chulo_verde.png" style="max-width: 50px"><p class="success_ask">¡Acertaste!</p>' +
+    		'<p class="classic_text">'+ $(this).attr("mensaje") +'</p><button class="boton_siguiente_trivia">Siguiente trivia</button>';
+    		$('.scenario_popup').append(image_result);
+
+    	} else {
+    		$('.scenario_popup_container').css("display", "flex");
+    		var image_result = '<img src="./img/equis.png" style="max-width: 50px"><p class="fail_ask">Fallaste XD</p>' +
+    		'<p class="classic_text">'+ $(this).attr("mensaje") +'</p><button class="boton_siguiente_trivia">Siguiente trivia</button>';
+    		$('.scenario_popup').append(image_result);
+
+    	}
+    });
+
+    $('.scenario_popup').on('click', '.boton_siguiente_trivia', function(){
+    	$('.scenario_popup').empty();
+    	$('.scenario_popup_container').css("display", "none");
+    	scenario_number++;
+    	$("#scenario_" + scenario_number).css("display", "flex");
+    	$("#scenario_" + (scenario_number - 1)).css("display", "none");
+    	$("#header_title").html("");
+    	$("#header_title").html(scenario_titles[scenario_number - 1]);
+    });
+
+    //WHY BUTTON
+    $('.why_button').click(function(){
+    	$('.scenario_popup_container').css("display", "flex");
+    	var reason_text = '';
+
+    	switch($(this).attr("cancion")){
+    		case "1":
+    			reason_text = '<p class="classic_text">Fue la primer canción que te dediqué y la que mas me ' + 
+    			'recuerda a ti en general, porque me haces sentir especial, siempre.</p>';
+				break;
+			case "2":
+				reason_text = '<p class="classic_text">Los conocí por ti y por esa misma razón me recuerdan a ' + 
+    			'ti, su canción más icónica para mi es esta, Red.</p>';
+    		case "3":
+    			reason_text = '<p class="classic_text">Mi canción favorita de Red Velvet, escucharla es pensar ' + 
+    			'acerca del amor, justo lo que  siento por ti.</p>';
+    		case "4":
+    			reason_text = '<p class="classic_text">Me pasa igual que con Red, es una canción que me recuerda ' + 
+    			'a ti, porque me mencionaste que te hace feliz y me encanta verte así.</p>';
+    	}
+
+    	reason_text += '<button class="cerrar_popup">Cerrar<button>';
+
+    	$('.scenario_popup').append(reason_text);
+    });
+
+    //Cerrar popup
+    $('.scenario_popup').on('click', '.cerrar_popup', function(){
+    	$('.scenario_popup').empty();
+    	$('.scenario_popup_container').css("display", "none");
     });
 });
